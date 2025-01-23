0170a804270ac1dd18b9f10947f7041c7e68bdf2
likithrajk <rajlikith1998@gmail.com>
2025-01-23 16:22:18 +0530

    enviroment,prod.ts file updated

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://us-central1-junkielabs-57977.cloudfunctions.net/apiPortfolio',
+  recaptcha: '6Lci95YiAAAAAOfkpaWwNSHiX5Ye7KT9ZOw-HYCv',
+  emailJs: {
+    serviceId: 'service_82ogguj',
+    templateId: 'template_cx3oagg',
+    publicKey: '0Y80JhNoPvNflhNce',
+  },
+};
+
+
